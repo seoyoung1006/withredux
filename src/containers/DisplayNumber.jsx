6ab3d0a5d351dc0a87c7010b486a98a792722693
toystory <74535088+seoyoung1006@users.noreply.github.com>
2022-08-03 18:12:46 +0900

    Use react-redux

--- src/containers/DisplayNumber.jsx
+++ src/containers/DisplayNumber.jsx
@@ -1,16 +1,19 @@
 import DisplayNumber from "../components/DisplayNumber";
-import React, { Component } from "react";
-import store from "../store";
+import { connect } from "react-redux";
+export default connect()(DisplayNumber);
 
-export default class extends Component{
-    state = {number:store.getState().number}
-    constructor(props){
-        super(props);
-        store.subscribe(function(){
-            this.setState({number: store.getState().number});
-        }.bind(this));
-    }
-    render(){
-        return <DisplayNumber number={this.state.number}></DisplayNumber>
-    }
-}
+// import React, { Component } from "react";
+// import store from "../store";
+
+// export default class extends Component{
+//     state = {number:store.getState().number}
+//     constructor(props){
+//         super(props);
+//         store.subscribe(function(){
+//             this.setState({number: store.getState().number});
+//         }.bind(this));
+//     }
+//     render(){
+//         return <DisplayNumber number={this.state.number}></DisplayNumber>
+//     }
+// }
