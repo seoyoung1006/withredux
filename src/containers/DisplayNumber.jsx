5dba50dfbed7ac4bf316ae8c33a760e3bd482c1b
toystory <74535088+seoyoung1006@users.noreply.github.com>
2022-08-03 16:54:38 +0900

    Make cotainer displaynumber

---
+++ src/containers/DisplayNumber.jsx
@@ -0,0 +1,8 @@
+import DisplayNumber from "../components/DisplayNumber";
+import React, { Component } from "react";
+
+export default class extends component{
+    render(){
+        return <DisplayNumber></DisplayNumber>
+    }
+}
