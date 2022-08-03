a5f49bb0142db70656db08778d461cd3589a99aa
toystory <74535088+seoyoung1006@users.noreply.github.com>
2022-08-03 15:29:42 +0900

    Use redux on react

--- src/components/AddNumber.jsx
+++ src/components/AddNumber.jsx
@@ -1,5 +1,6 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React, {Component} from "react";
-
+import store from "../store";
 class AddNumber extends Component{
     state = {size:1}
     render(){
@@ -7,7 +8,7 @@ class AddNumber extends Component{
             <div>
                 <h1>Add Number</h1>
                 <input type="button" value="+" onClick={function(){
-                    this.props.onClick(this.state.size);
+                    store.dispatch({type:'INCREMENT', size:this.state.size});
                 }.bind(this)}></input>
                 <input type="text" value={this.state.size} onChange={function(e){
                     this.setState({size:Number(e.target.value)});
@@ -15,6 +16,6 @@ class AddNumber extends Component{
             </div>
         )
     }
-}
+} 
 
 export default AddNumber;
